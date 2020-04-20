c610ce4a86b5205a1e0e24a788df70b75fd3f40a
Philippe Cholet <44676486+Philippe-Cholet@users.noreply.github.com>
2020-04-20 12:54:07 +0200

    checkers.float_comparison(5)
    It would allow `numpy.linalg.det` to be a solution without having to round the solution.

--- verification/referee.py
+++ verification/referee.py
@@ -1,9 +1,13 @@
 from checkio.signals import ON_CONNECT
 from checkio import api
 from checkio.referees.io import CheckiOReferee
+from checkio.referees import checkers
 
 from tests import TESTS
 
 api.add_listener(
     ON_CONNECT,
-    CheckiOReferee(tests=TESTS).on_ready)
+    CheckiOReferee(
+        tests=TESTS,
+        checker=checkers.float_comparison(5),
+    ).on_ready)
